abec56c586edb3db67dab2e1a474254f188afe66
zanelkt <zanelockett@gmail.com>
2018-07-11 16:31:34 +1200

    sprint 5.3

--- classes.js
+++ classes.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+   one()
+   two() 
+   three()
+   makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -19,7 +21,18 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
-
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
-
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('brown')
+}
 // CREATE FUNCTION makeVisible HERE
+
+function makeVisible()  {
+  var visible = document.getElementsByClassName('invisible')[0]
+  visible.classList.add('visible')
+}
